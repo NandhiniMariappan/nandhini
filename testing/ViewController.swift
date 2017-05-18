8d392921836c9995d4e89d2257ed5f43486b763d
Deepak <deepak@deepakmac.local>
2017-05-18 15:31:50 +0530

    Initial Commit

---
+++ testing/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testing
+//
+//  Created by Deepak on 18/05/2016 A.
+//  Copyright © 2017 Adyog. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
